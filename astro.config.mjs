0d93db781cb28f552145ff455578fc19095f4291
Fafifo92 <franconuezm@gmail.com>
2026-05-26 12:03:23 -0500

    fix: share pop up

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://symphonious-cascaron-d57286.netlify.app',
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
